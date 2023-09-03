1f889cd3f36269b45ad6d80ea1d9e17cc71ddb47
Deight <d8corp@mail.ru>
2023-09-03 22:54:38 +0300

    fix boolean rule

--- src/plugins/schema/boolean/boolean.ts
+++ src/plugins/schema/boolean/boolean.ts
@@ -1,7 +1,7 @@
 import { type HandlerPlugin } from 'innet'
-import { useContext, useProps } from '@innet/jsx'
+import { useProps } from '@innet/jsx'
 
-import { paramContext, useBlock, useRule, useSchemaType } from '../../../hooks'
+import { useBlock, useRule, useSchemaType } from '../../../hooks'
 import { useParentRule } from '../../../hooks/useParentRule'
 import { type BaseSchemaProps } from '../../../types'
 import { defaultTo, pipe, type Rule } from '../../../utils'
@@ -14,7 +14,6 @@ export const boolean: HandlerPlugin = () => {
   useBlock('path')
   const props = useProps<BooleanProps>()
   useSchemaType('boolean', props)
-  const param = useContext(paramContext)
 
   const rules: Rule[] = []
 
@@ -22,7 +21,7 @@ export const boolean: HandlerPlugin = () => {
     rules.push(defaultTo(props.default))
   }
 
-  rules.push(param ? val => val === 'true' || (val === 'false' ? false : Boolean(val)) : Boolean)
+  rules.push(val => val === 'true' || (val === 'false' ? false : Boolean(val)))
 
   if (props?.default === undefined) {
     const parentRule = useParentRule()
