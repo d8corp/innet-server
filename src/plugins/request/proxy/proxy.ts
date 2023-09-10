9f6ae53f521d58b00316c381f93d97730289a8c9
Deight <d8corp@mail.ru>
2023-09-10 20:20:46 +0300

    add onProxyRes for proxy

--- src/plugins/request/proxy/proxy.ts
+++ src/plugins/request/proxy/proxy.ts
@@ -1,5 +1,6 @@
 import { type HandlerPlugin } from 'innet'
 import { useProps } from '@innet/jsx'
+import { type IncomingMessage } from 'http'
 import httpProxy from 'http-proxy'
 
 import { useRequest, useResponse, useThrow } from '../../../hooks'
@@ -7,10 +8,11 @@ import { useRequest, useResponse, useThrow } from '../../../hooks'
 export interface ProxyProps {
   to: string
   secure?: boolean
+  onProxyRes?: (res: IncomingMessage) => void
 }
 
 export const proxy: HandlerPlugin = () => {
-  const { to, secure } = useProps<ProxyProps>()
+  const { to, secure, onProxyRes } = useProps<ProxyProps>()
   const req = useRequest()
   const res = useResponse()
 
@@ -21,6 +23,10 @@ export const proxy: HandlerPlugin = () => {
   const proxyServer = httpProxy.createProxyServer({})
 
   delete req.headers.host
+
+  if (onProxyRes) {
+    proxyServer.on('proxyRes', onProxyRes)
+  }
 
   proxyServer.web(req, res, { target: to, secure })
 }
