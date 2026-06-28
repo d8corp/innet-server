4205bb572cc3875bf7812f79355c4898ca73ca62
Deight <d8corp@mail.ru>
2026-06-28 09:41:07 +0300

    Update readme

--- src/types/appTypes.ts
+++ src/types/appTypes.ts
@@ -94,40 +94,145 @@ export type ApiErrorSchemaRefs = Record<ApiErrorValue, string>
 export type SchemaValues<T> = (T extends (number | string) ? Record<T, string> : never) | T[]
 
 export interface BaseSchemaProps<T, D = T> {
-  /** Default value for the schema */
+  /**
+   * Default value for the schema.
+   *
+   * @example
+   * ```tsx
+   * <number default={1} />
+   * ```
+   */
   default?: D
 
-  /** Indicates that the schema is deprecated */
+  /**
+   * Indicates that the schema is deprecated.
+   *
+   * @example
+   * ```tsx
+   * <string deprecated />
+   * ```
+   */
   deprecated?: boolean
 
-  /** Schema description in CommonMark format */
+  /**
+   * Schema description in CommonMark format.
+   *
+   * @example
+   * ```tsx
+   * <string description='User email address in format `name@domain.com`' />
+   * ```
+   */
   description?: string
 
-  /** Example value */
+  /**
+   * Example value.
+   *
+   * @example
+   * ```tsx
+   * <string example='user@example.com' />
+   * ```
+   */
   example?: T
 
-  /** Array of example values */
+  /**
+   * Array of example values.
+   *
+   * @example
+   * ```tsx
+   * <string examples={['john@example.com', 'jane@test.com']} />
+   * ```
+   */
   examples?: T[]
 
-  /** Indicates that the value can be null */
+  /**
+   * Indicates that the value can be null.
+   *
+   * @example
+   * ```tsx
+   * <string nullable />
+   * ```
+   */
   nullable?: false
 
-  /** Indicates that the value is read-only */
+  /**
+   * Indicates that the value is read-only.
+   *
+   * @example
+   * ```tsx
+   * <object ref='Todo'>
+   *   id: <uuid readOnly />
+   *   title: <string />
+   *   done: <boolean />
+   * </object>
+   * ```
+   */
   readOnly?: boolean
 
-  /** Reference to a schema component in OpenAPI */
+  /**
+   * Reference to a schema component in OpenAPI.
+   *
+   * @example
+   * ```tsx
+   * <object ref='Todo'>
+   *   id: <uuid />
+   *   title: <string />
+   *   done: <boolean />
+   * </object>
+   * ```
+   */
   ref?: string
 
-  /** Schema title */
+  /**
+   * Schema title.
+   *
+   * @example
+   * ```tsx
+   * <string title='User Email' />
+   * ```
+   */
   title?: string
 
-  /** Specific value */
+  /**
+   * Specific value.
+   *
+   * @example
+   * ```tsx
+   * <string value='active' />
+   * ```
+   */
   value?: T
 
-  /** Allowed values (enum) or dictionary of values with descriptions */
+  /**
+   * Allowed values (array) or dictionary of values with descriptions.
+   *
+   * @example
+   * ```tsx
+   * <string values={['active', 'inactive', 'pending']} />
+   * ```
+   * @example
+   * ```tsx
+   * <string
+   *   values={{
+   *     active: 'Main status',
+   *     inactive: 'Deleted',
+   *     pending: 'Waiting for result',
+   *   }}
+   * />
+   */
   values?: SchemaValues<T>
 
-  /** Indicates that the value is write-only */
+  /**
+   * Indicates that the value is write-only.
+   *
+   * @example
+   * ```tsx
+   * <object ref='User'>
+   *   id: <uuid readOnly />
+   *   name: <string />
+   *   password: <string writeOnly />
+   * </object>
+   * ```
+   */
   writeOnly?: boolean
 }
 
