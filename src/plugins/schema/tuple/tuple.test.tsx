2fb4891db85bcc63f03d5fc57dce5af4d4a83a5f
Deight <d8corp@mail.ru>
2026-07-01 14:14:23 +0300

    Add test

---
+++ src/plugins/schema/tuple/tuple.test.tsx
@@ -0,0 +1,48 @@
+import { useBody } from '../../../hooks'
+import { runTest } from '../../../testing'
+
+describe('tuple', () => {
+  it('should convert a field to tuple', async () => {
+    const log = jest.fn()
+
+    function Runtime () {
+      const body = useBody()
+      log(body)
+
+      return <success>{body}</success>
+    }
+
+    const stop = await runTest((onStart, onEnd) => (
+      <server onClose={onEnd} onStart={onStart}>
+        <api>
+          <endpoint method='post' path='/test'>
+            <body>
+              <object>
+                name: <tuple><string /></tuple>
+              </object>
+            </body>
+            <return>
+              <Runtime />
+            </return>
+          </endpoint>
+        </api>
+        <return><error /></return>
+      </server>),
+    )
+
+    const res = await fetch('http://localhost/test', {
+      body: JSON.stringify({
+        name: 123,
+      }),
+      headers: { 'content-type': 'application/json' },
+      method: 'post',
+    })
+
+    const json = await res.json()
+
+    await stop()
+
+    expect(log).toHaveBeenCalledTimes(1)
+    expect(json).toEqual({ name: ['123'] })
+  })
+})
